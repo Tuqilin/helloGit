c147e5ba18a6f4b13c3b0a9fea3196a1721ef34c
Tuqilin <1593751661@qq.com>
2021-10-17 20:42:10 +0800

    更新1

---
+++ HelloGit/src/main/java/com/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @author TQL  2021/10/17
+ * @Description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("更新1");
+    }
+}
